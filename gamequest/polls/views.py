d2f9d47df4b48edf7783ba49f8fc6a9afb3db012
Mikescops <mikescops@pixelswap.fr>
2014-02-26 13:06:06 +0000

    Views Update

--- gamequest/polls/views.py
+++ gamequest/polls/views.py
@@ -5,8 +5,10 @@ from gamequest.polls.models import Poll
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    context = RequestContext(request, {
+        'latest_poll_list': latest_poll_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
